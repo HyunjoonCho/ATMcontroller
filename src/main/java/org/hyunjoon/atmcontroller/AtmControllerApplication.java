f7bad9fb8b0fb9984e3a35e414060d1102a43bb9
HyunjoonCho <jhj09016@gmail.com>
2021-02-28 23:27:06 +0900

    Spring Boot Starter

---
+++ src/main/java/org/hyunjoon/atmcontroller/AtmControllerApplication.java
@@ -0,0 +1,13 @@
+package org.hyunjoon.atmcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hyunjoon/atmcontroller/AtmControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyunjoon.atmcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
